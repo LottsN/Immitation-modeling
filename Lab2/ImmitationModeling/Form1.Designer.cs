6ee1963955771ee116f72e727b7ac4ac9a3e3794
LottsN <80810726+LottsN@users.noreply.github.com>
2022-04-25 08:36:52 +0300

    Add files via upload

---
+++ Lab2/ImmitationModeling/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ImmitationModeling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.edEuroPrice = new System.Windows.Forms.NumericUpDown();
+            this.btCalculate = new System.Windows.Forms.Button();
+            this.edDollarPrice = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btClean = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edEuroPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDollarPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 74);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            series1.EmptyPointStyle.Name = "ser";
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "0.00";
+            series1.Legend = "Legend1";
+            series1.Name = "Dollar";
+            series1.YValuesPerPoint = 4;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series2.IsValueShownAsLabel = true;
+            series2.LabelFormat = "0.00";
+            series2.Legend = "Legend1";
+            series2.Name = "Euro";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1203, 556);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btClean);
+            this.panel1.Controls.Add(this.edEuroPrice);
+            this.panel1.Controls.Add(this.btCalculate);
+            this.panel1.Controls.Add(this.edDollarPrice);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1203, 83);
+            this.panel1.TabIndex = 1;
+            // 
+            // edEuroPrice
+            // 
+            this.edEuroPrice.DecimalPlaces = 2;
+            this.edEuroPrice.Location = new System.Drawing.Point(292, 46);
+            this.edEuroPrice.Name = "edEuroPrice";
+            this.edEuroPrice.Size = new System.Drawing.Size(120, 20);
+            this.edEuroPrice.TabIndex = 12;
+            this.edEuroPrice.Value = new decimal(new int[] {
+            8510,
+            0,
+            0,
+            131072});
+            // 
+            // btCalculate
+            // 
+            this.btCalculate.Location = new System.Drawing.Point(514, 15);
+            this.btCalculate.Name = "btCalculate";
+            this.btCalculate.Size = new System.Drawing.Size(336, 53);
+            this.btCalculate.TabIndex = 8;
+            this.btCalculate.Text = "Start/Stop";
+            this.btCalculate.UseVisualStyleBackColor = true;
+            this.btCalculate.Click += new System.EventHandler(this.btCalculate_Click);
+            // 
+            // edDollarPrice
+            // 
+            this.edDollarPrice.DecimalPlaces = 2;
+            this.edDollarPrice.Location = new System.Drawing.Point(78, 46);
+            this.edDollarPrice.Name = "edDollarPrice";
+            this.edDollarPrice.Size = new System.Drawing.Size(120, 20);
+            this.edDollarPrice.TabIndex = 7;
+            this.edDollarPrice.Value = new decimal(new int[] {
+            7565,
+            0,
+            0,
+            131072});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(254, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Euro:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Initial Prices:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dollar:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btClean
+            // 
+            this.btClean.Location = new System.Drawing.Point(867, 15);
+            this.btClean.Name = "btClean";
+            this.btClean.Size = new System.Drawing.Size(324, 53);
+            this.btClean.TabIndex = 13;
+            this.btClean.Text = "Clean";
+            this.btClean.UseVisualStyleBackColor = true;
+            this.btClean.Click += new System.EventHandler(this.btClean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1203, 630);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edEuroPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDollarPrice)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edDollarPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btCalculate;
+        private System.Windows.Forms.NumericUpDown edEuroPrice;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btClean;
+    }
+}
+
---
+++ Lab2/ImmitationModeling/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImmitationModeling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double k = 0.20;
+        double DollarPrice, EuroPrice;
+        bool hadRun = false;
+        double T = 0;
+        Random rnd = new Random();
+
+        private void btCalculate_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                if (!hadRun)
+                {
+                    chart1.Series[0].Points.Clear();
+                    chart1.Series[1].Points.Clear();
+                    chart1.ChartAreas[0].AxisX.Minimum = 0;
+
+                    DollarPrice = (double)edDollarPrice.Value;
+                    EuroPrice = (double)edEuroPrice.Value;
+
+                    chart1.Series[0].Points.AddXY(0, DollarPrice);
+                    chart1.Series[1].Points.AddXY(0, EuroPrice);
+                    hadRun = true;
+                    timer1.Start();
+                }
+                else
+                {
+
+                    timer1.Start();
+                }
+
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void btClean_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            T = 0;
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            hadRun = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            T = T + 1;
+            DollarPrice = DollarPrice * (1 + k * (rnd.NextDouble() - 0.5));
+            EuroPrice = EuroPrice * (1 + k * (rnd.NextDouble() - 0.5));
+
+            chart1.Series[0].Points.AddXY(T, DollarPrice);
+            chart1.Series[1].Points.AddXY(T, EuroPrice);
+            if (chart1.Series[0].Points.Count >= 30) 
+            {
+                chart1.ChartAreas[0].AxisX.Minimum = chart1.Series[0].Points.Count - 30;
+            }
+        }
+    }
+}
